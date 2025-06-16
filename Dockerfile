f7302441f07cd8ac50905eb223e72829baae6db4
Kirill Balezin <k.balezin@lipt-soft.ru>
2025-06-16 13:28:24 +0300

    f

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,3 @@ FROM python:3.11-slim-buster
 WORKDIR /app
 
 COPY . /app
-
-# CMD ["python", "-m", "unittest", "discover", "-s", "tests"]
--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Calculator:
     def __init__(self):
         pass
@@ -13,3 +16,24 @@ class Calculator:
 
     def divide(self, a=0, b=0):
         return a / b
+
+
+def main():
+    log = logging.getLogger("main")
+    log.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+    calc = Calculator()
+    log.info(f"add: {calc.add(1, 2)}")
+    log.info(f"subtract: {calc.subtract(1, 2)}")
+    log.info(f"multiply: {calc.multiply(1, 2)}")
+    log.info(f"divide: {calc.divide(1, 2)}")
+
+
+if __name__ == "__main__":
+    main()
